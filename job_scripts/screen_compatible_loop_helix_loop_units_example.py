8e0c3e29788563d1e7bd2ecec42bafcb672ba1b2
xingjiepan <xingjiepan@gmail.com>
2018-09-17 08:57:20 -0700

    Update the script screen_compatible_loop_helix_loop_units_example.py

--- job_scripts/screen_compatible_loop_helix_loop_units_example.py
+++ job_scripts/screen_compatible_loop_helix_loop_units_example.py
@@ -10,31 +10,35 @@ from pyrosetta import rosetta
 import loop_helix_loop_reshaping as LHLR
 
 
-def screen_compatible_loop_helix_loop_units(data_path, lhl_units_path, input_pdb, input_insertion_points_file, num_jobs, job_id):
+def screen_compatible_loop_helix_loop_units(data_path, lhl_units_path, input_pdb, input_insertion_points_file, num_jobs, job_id, symmetric_lists=None):
     
     # Load insertion points
     
     with open(input_insertion_points_file, 'r') as f:
         insertion_points = json.load(f)
-    
+   
+    if symmetric_lists is None:
+        symmetric_lists = [[i] for i in range(len(insertion_points))]
+
     # Load the lhl_units
     
     lhl_units = []
 
-    for i in range(len(insertion_points)):
+    for i in range(len(symmetric_lists)):
         lhl_units.append([])
-
-        for lf in os.listdir(lhl_units_path):
-            if lf.startswith('selected_lhl_units_{0}'.format(i)):
-                with open(os.path.join(lhl_units_path, lf), 'r') as f:
-                    lhl_units[i] += json.load(f)
-   
+    
+        for j in symmetric_lists[i]:
+            for lf in os.listdir(lhl_units_path):
+                if lf.startswith('selected_lhl_units_{0}'.format(j)):
+                    with open(os.path.join(lhl_units_path, lf), 'r') as f:
+                        lhl_units[i] += json.load(f)
+    
     # Load the pose
     
     pose = rosetta.core.import_pose.pose_from_file(input_pdb)
 
     LHLR.screen_compatible_loop_helix_loop_units.screen_compatible_loop_helix_loop_units(
-            data_path, pose, insertion_points, lhl_units, num_jobs, job_id)
+            data_path, pose, insertion_points, lhl_units, num_jobs, job_id, symmetric_lists=symmetric_lists)
     
 
 
@@ -55,5 +59,6 @@ if __name__ == '__main__':
     input_insertion_points_file = 'test_inputs/2lv8_insertion_points.json'
 
     screen_compatible_loop_helix_loop_units(data_path, lhl_units_path, 
-            input_pdb, input_insertion_points_file, num_jobs, job_id)
+            input_pdb, input_insertion_points_file, num_jobs, job_id,
+	    symmetric_lists=None)
 
